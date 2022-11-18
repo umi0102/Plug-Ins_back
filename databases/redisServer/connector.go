2255823b9859d2ec2f9c3af18cbabc359b3d8545
umi0102 <118367161+umi0102@users.noreply.github.com>
2022-11-18 23:21:18 +0800

    Update connector.go

--- databases/redisServer/connector.go
+++ databases/redisServer/connector.go
@@ -14,7 +14,7 @@ func init() {
 		MaxActive:   0,   //表示和数据库最大链接数，0表示，并发不限制数量
 		IdleTimeout: 100, //最大空闲时间，用完链接后100秒后就回收到链接池
 		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "127.0.0.1:6379", redis.DialDatabase(0), redis.DialPassword("Fjw0102*"))
+			return redis.Dial("tcp", "127.0.0.1:6379", redis.DialDatabase(0), redis.DialPassword("???????"))
 		},
 	}
 }
