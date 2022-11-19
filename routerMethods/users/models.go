23f0849319bfc6758b95226df322939249583660
weihahaha <707750760@qq.com>
2022-11-19 16:22:21 +0800

    //

--- routerMethods/users/models.go
+++ routerMethods/users/models.go
@@ -2,7 +2,7 @@ package users
 
 type LoginRequest struct {
 	Phone    string `json:"phone" binding:"required,min=10,max=14"`
-	Password string `json:"password" binding:"required,min=7,max=20"`
+	Password string `json:"password" binding:"max=20"`
 	Code     string `json:"code" binding:"max=10"`
 }
 
