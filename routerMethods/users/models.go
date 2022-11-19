68b77995a0844206dfcd089f2691a899926a03d1
weihahaha <707750760@qq.com>
2022-11-19 19:57:32 +0800

    //

--- routerMethods/users/models.go
+++ routerMethods/users/models.go
@@ -6,8 +6,3 @@ type LoginRequest struct {
 	Name     string `json:"name" binding:"max=50"`
 	Code     string `json:"code" binding:"max=10"`
 }
-
-type LoginByCodeRequest struct {
-	Phone string `json:"phone" binding:"required,min=10,max=14"`
-	Code  string `json:"code" binding:"required,min=1,max=10"`
-}
